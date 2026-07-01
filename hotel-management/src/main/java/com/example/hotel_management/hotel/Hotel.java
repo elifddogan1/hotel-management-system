659aa9b1cac07adbd7ba0efbcad0ff4e4d96e4e7
Elif Dogan <elifddogan1@gmail.com>
2026-07-01 12:25:40 +0300

    feat(hotel): implement hotel entity, repository, service and controller

---
+++ hotel-management/src/main/java/com/example/hotel_management/hotel/Hotel.java
@@ -0,0 +1,41 @@
+package com.example.hotel_management.hotel;
+
+import java.util.List;
+
+import com.example.hotel_management.room.Room;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "hotels")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    private String location;
+
+    private String contactInfo;
+
+    @OneToMany(mappedBy = "hotel", cascade = CascadeType.ALL)
+    List<Room> rooms;
+}
