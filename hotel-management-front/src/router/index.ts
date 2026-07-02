c565049eb4df87f0ee3f79e9ec60e00cf981f99f
Elif Dogan <elifddogan1@gmail.com>
2026-07-02 16:36:11 +0300

    feat(frontend): configure routing for guest, room, hotel, and reservation views

--- hotel-management-front/src/router/index.ts
+++ hotel-management-front/src/router/index.ts
@@ -1,12 +1,22 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 import HotelView from '../views/HotelView.vue';
-
+import HotelDetailView from '../views/HotelDetailView.vue';
+import RoomView from '../views/RoomView.vue';
+import GuestView from '../views/GuestView.vue';
+import ReservationView from '@/views/ReservationView.vue';
 
 const routes: Array<RouteRecordRaw> = [
+  { path: '/', name: 'home', component: HomeView },
+  { path: '/hotels', name: 'hotels', component: HotelView },
+  { path: '/hotels/:id', name: 'hotel-detail', component: HotelDetailView },
+  { path: '/rooms', name: 'rooms', component: RoomView },
+  { path: '/reservation', name: 'reservation', component: ReservationView }, // YENİ ROTA
+  { path: '/guests', name: 'guests', component: GuestView },
   {
-    path: '/',
-    name: 'hotels',
-    component: HotelView
+    path: '/hotels/:hotelId/rooms/:roomId',
+    name: 'room-detail',
+    component: () => import('../views/RoomDetailView.vue')
   }
 ];
 
