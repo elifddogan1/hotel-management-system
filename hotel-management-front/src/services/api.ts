77b52c0337780b95cc13d9eeeb091d992f5723cc
Elif Dogan <elifddogan1@gmail.com>
2026-07-03 20:02:37 +0300

    ports number fixed

--- hotel-management-front/src/services/api.ts
+++ hotel-management-front/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api', // Dockerized Backend URL'imiz
+    baseURL: 'http://34.40.79.243:8080/api', // Dockerized Backend URL'imiz
     headers: {
         'Content-Type': 'application/json',
     },
