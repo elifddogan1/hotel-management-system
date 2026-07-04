56dbc8e9c7273ba10c26f75ca193a17a0ffbe430
Elif Dogan <elifddogan1@gmail.com>
2026-07-04 16:06:00 +0300

    baseurl api changed

--- hotel-management-front/src/services/api.ts
+++ hotel-management-front/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://34.40.79.243:8080/api', // Dockerized Backend URL'imiz
+    baseURL: 'http://hotelmanagement.duckdns.org/api', // Dockerized Backend URL'imiz
     headers: {
         'Content-Type': 'application/json',
     },
