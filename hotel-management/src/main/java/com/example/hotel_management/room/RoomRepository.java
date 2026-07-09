695dca119e719f3838aca32413a57a2781b2b46b
Elif Dogan <elifddogan1@gmail.com>
2026-07-09 17:02:08 +0300

    feat(room): extend RoomRepository with JpaSpecificationExecutor

--- hotel-management/src/main/java/com/example/hotel_management/room/RoomRepository.java
+++ hotel-management/src/main/java/com/example/hotel_management/room/RoomRepository.java
@@ -3,10 +3,11 @@ package com.example.hotel_management.room;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoomRepository extends JpaRepository<Room, Long> {
+public interface RoomRepository extends JpaRepository<Room, Long>, JpaSpecificationExecutor<Room> {
     List<Room> findByHotelId(Long hotelId);
 
     List<Room> findByHotelIdAndMaxCapacityGreaterThanEqual(Long hotelId, int capacity);
